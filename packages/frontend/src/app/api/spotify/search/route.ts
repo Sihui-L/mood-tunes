7a4946fc26171b15140457b7e492e327cce6a452
Sihui-L <sihuili.work@gmail.com>
2025-02-11 21:22:02 +0000

    Make song playble

--- packages/frontend/src/app/api/spotify/search/route.ts
+++ packages/frontend/src/app/api/spotify/search/route.ts
@@ -1,3 +1,5 @@
+import { fetchSpotifyToken } from "../../../../lib/fetchSpotifyToken";
+
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const query = url.searchParams.get("q");
@@ -6,24 +8,8 @@ export async function GET(req: Request) {
     return new Response(JSON.stringify({ error: "No search query provided" }), { status: 400 });
   }
 
-  const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
-  const clientSecret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
-  const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+  const accessToken = await fetchSpotifyToken();
 
-  // Get Access Token
-  const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${credentials}`,
-    },
-    body: new URLSearchParams({ grant_type: "client_credentials" }),
-  });
-
-  const tokenData = await tokenResponse.json();
-  const accessToken = tokenData.access_token;
-
-  // Search for a song
   const searchResponse = await fetch(
     `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track&limit=5`,
     {
